3c8c8b6bc0ce928093734ab75400e6d15d533758
Magnas-Manuel <41764909+Magnas-Manuel@users.noreply.github.com>
2018-07-26 20:45:43 -0500

    Update Conexion.php

--- persistencia/basedatos/Conexion.php
+++ persistencia/basedatos/Conexion.php
@@ -7,7 +7,7 @@ use PDO;
 class Conexion {
 
     public function conectar() {
-        $cnn = new PDO('pgsql:host=den1.mysql6.gear.host;dbname=personalPpro;', 'personalpro', 'Xb5Bq!vpGK-9');
+        $cnn = new PDO('mysql:host=den1.mysql6.gear.host;dbname=personalpro;', 'personalpro', 'Xb5Bq!vpGK-9');
         $cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $cnn;
     }
